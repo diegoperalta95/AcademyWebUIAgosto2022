19bcdf0b978a67ffd4d63a49b14bff856f7bb67f
Diego Peralta <diegooperalta95@gmail.com>
2022-08-13 16:04:57 -0300

    v0.2 - Shop
    Hecho shop, faltaria el modal

---
+++ shop/index.js
@@ -0,0 +1,25 @@
+/*
+const sectionModal = document.getElementById("section-article-modal");
+const ShowModal = function () {
+    sectionModal.setAttribute("class", "show-modal")
+}
+const HideModal = function () {
+    sectionModal.setAttribute("class", "hide-modal")
+}
+document.addEventListener("click",
+    function (event) {
+        if (event.target.matches("#backToShopBtn") || !event.target.closest(".contenedor")) {
+            HideModal()
+        }
+    },
+    false
+)
+document.addEventListener("click", 
+function (e) {
+    if(e.target.matches('.articleCard')){
+        ShowModal();
+    }
+});
+
+ShowModal();
+*/
